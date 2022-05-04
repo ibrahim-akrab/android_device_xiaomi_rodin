cddb724b9fa737bb314b24c135204afdbdf820e7
SamarV-121 <samarvispute121@pm.me>
2022-05-04 17:18:03 +0100

    rodin: Switch to OSS Lights AIDL from RM6785
    [LinkBoi00] Rebrand to cannon and update license boilerplates
    
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Co-authored-by: vasishath <vasishath@gmail.com>
    Co-authored-by: daniml3 <daniel@danielml.dev>
    Change-Id: Iec09b8f30e396f572dabd06515bbe2ac51d5addd
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -221,7 +221,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.lineage
+    android.hardware.light-service.rodin
 
 # Media
 PRODUCT_COPY_FILES += \
