2941c623545a3ea3785bf0ff5d1dae48a792c91b
LuK1337 <priv.luk@gmail.com>
2023-10-26 15:21:23 +0100

    rodin: sepolicy: Label wakeup nodes
    for i in $(realpath /sys/class/wakeup/*); do if ! echo "$(ls -dZ $i)" | grep -q sysfs_wakeup; then echo genfscon sysfs $(echo $i | sed -e 's|/sys||g' -e 's|wakeup/wakeup.|wakeup|g' -e 's|wakeup[0-9]*|wakeup|g') u:object_r:sysfs_wakeup:s0; fi; done | sort -u
    
    Change-Id: Id70f4e725f8925b31c0007fae5eb98756ddcd410
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,7 @@ BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
