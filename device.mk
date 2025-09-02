f4958ab92225607a374a14a279b103a1df70e946
Ido <dev.xyzuniverse@gmail.com>
2025-09-02 11:46:53 +0200

    rodin: Exclude AudioFX
    Change-Id: I18f76b86215ccf0880209a21f563760fcf98ee18
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -90,6 +90,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_vendor=true
 
 # Audio
+TARGET_EXCLUDES_AUDIOFX := true
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.1-impl \
