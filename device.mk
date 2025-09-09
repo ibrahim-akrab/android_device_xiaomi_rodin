aa84b643de5a69db637a8bb5a16d86da1f2de6d5
Ido <dev.xyzuniverse@gmail.com>
2025-09-09 05:28:28 +0200

    Revert "rodin: Switch to prebuilt lights service"
    This reverts commit d9754bdce82341b2f9838ba60d60a500937b1998.
    
    Change-Id: Iab5ed3f33be3fdb301a0d2f16f80c1478db61951
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -233,6 +233,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
